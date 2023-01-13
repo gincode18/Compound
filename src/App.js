5398a1f7a2d34c4976843263b20ff25a7a1016ac
gincode <vishalkamboj9211@gmail.com>
2023-01-14 00:52:08 +0530

    finally hogya bc

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./index.css"
-import Blogs from "./routes/Blogs";
+import Blogs from "./pages/Blog";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
 import Team from "./routes/Team";
+import SinglePost from "./pages/SinglePost";
+import Error from "./pages/Error";
 import { Route,Routes } from "react-router-dom";
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
     <Routes>
       <Route path='/'element ={<Home/>}/>
       <Route path='/blog'element ={<Blogs/>}/>
+      <Route path="/blog/:slug" element={<SinglePost />}></Route>
       <Route path='/login'element ={<Login/>}/>
+      <Route path="*" element={ <Error />}></Route>
       <Route path='/team'element ={<Team/>}/>
     </Routes>
     </>
---
+++ src/client.js
@@ -0,0 +1,10 @@
+import client from "@sanity/client"
+
+export default client({
+  projectId: "e0baqmhm",
+  dataset: "production",
+  useCdn: true,
+  apiVersion: "2021-08-05",
+  token:"sk1jf0sRLHeEFLXkGoYE6qLYoUvymCJZuMQh5DFGdqvj9rmmD0HbI0PZjkhxIwqPkFHAV3Vem3ij3lpvsPYrPfBF8DRi4N9mRrqaL3KFYjOikk9lOvuYOYgKavcCUZpj53mxkdnb0gd19hLAOZyOGIm0RZrKWyJoCKDBQxqvMmzxu58cPlrO",
+
+})
