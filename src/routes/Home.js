bb9439d18898eb637a385d7d215a9f51fc4bb109
Aman <krishanaman14@gmail.com>
2023-01-13 16:29:28 +0530

    jhv

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Footer from '../Components/Footer'
+import Help from '../Components/Help'
 import Landing from '../Components/Landing'
 import Navbar from '../Components/Navbar'
 
@@ -7,6 +9,9 @@ function Home() {
     <div>
       <Navbar/>
      <Landing/>
+     <Help/>
+     <Footer/>
+
     </div>
   )
 }
